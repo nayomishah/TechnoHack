807d4af0da36c49bd61a228cb082f6f4fb3d842a
nayomishah <nayomishah999@gmail.com>
2025-08-02 11:30:38 +0530

    header-footer
    header-footer changes with app changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 import HomePage from './Components/Home/HomePage';
+
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Header />
+
+      <div className="app">
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+
+      </div>
+
+      <Footer />
+    </BrowserRouter>
   );
 }
 
